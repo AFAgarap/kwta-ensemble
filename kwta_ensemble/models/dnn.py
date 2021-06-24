d87f4681e24111f7693b0a36610e4fb93674f70f
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-24 18:09:01 +0800

    add feed-forward network implementation

---
+++ kwta_ensemble/models/dnn.py
@@ -0,0 +1,86 @@
+# Winner-Take-All Ensemble Neural Network
+# Copyright (C) 2021  Abien Fred Agarap
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""Implementation of feed-forward neural network"""
+from typing import List, Tuple
+import torch
+
+
+class DNN(torch.nn.Module):
+    """
+    A feed-forward neural network that optimizes
+    softmax cross entropy using a gradient-based method.
+    """
+
+    def __init__(
+        self,
+        units: List or Tuple = [(784, 500), (500, 500), (500, 10)],
+        device: torch.device = torch.device(
+            "cuda:0" if torch.cuda.is_available() else "cpu"
+        ),
+    ):
+        """
+        Constructs a feed-forward neural network.
+
+        Parameters
+        ----------
+        units: list or tuple
+            An iterable that consists of the number of units in each hidden layer.
+        device: torch.device
+            The device to use for model computations.
+        """
+        super().__init__()
+        self.layers = torch.nn.ModuleList(
+            [
+                torch.nn.Linear(in_features, out_features)
+                for in_features, out_features in units
+            ]
+        )
+
+        for index, layer in enumerate(self.layers):
+            if index < (len(self.layers) - 1) and isinstance(layer, torch.nn.Linear):
+                torch.nn.init.kaiming_normal_(layer.weight, nonlinearity="relu")
+            elif index == (len(self.layers) - 1) and isinstance(layer, torch.nn.Linear):
+                torch.nn.init.xavier_uniform_(layer.weight)
+
+        self.device = device
+        self.to(self.device)
+
+    def forward(self, features: torch.Tensor) -> torch.Tensor:
+        """
+        Defines the forward pass by the model.
+
+        Parameter
+        ---------
+        features: torch.Tensor
+            The input features.
+        Returns
+        -------
+        logits: torch.Tensor
+            The model output.
+        """
+        if len(features.shape) > 2:
+            features = features.view(features.shape[0], -1)
+        activations = {}
+        for index, layer in enumerate(self.layers):
+            if index == 0:
+                activations[index] = torch.relu(layer(features))
+            elif index == len(self.layers) - 1:
+                activations[index] = layer(activations.get(index - 1))
+            else:
+                activations[index] = torch.relu(layer(activations.get(index - 1)))
+        logits = activations.get(len(activations) - 1)
+        return logits
