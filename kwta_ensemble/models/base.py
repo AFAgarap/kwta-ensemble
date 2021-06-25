eadedf74286951ab1e473b714fb5d0174d74c880
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-25 16:58:34 +0800

    add base class

---
+++ kwta_ensemble/models/base.py
@@ -0,0 +1,31 @@
+# Winner-Take-All Ensemble Neural Network
+# Copyright (C) 2021  Abien Fred Agarap
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""Implementation of model super class"""
+from typing import Dict, Tuple
+
+import torch
+
+
+class Model(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, features: torch.Tensor) -> torch.Tensor:
+        raise NotImplementedError
+
+    def epoch_train(self, data_loaders: Dict, phase: str) -> Tuple[float, float]:
+        raise NotImplementedError
