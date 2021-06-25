734717bd50986fe6675958773130fce5fd31d73b
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-25 17:00:47 +0800

    add training function

--- kwta_ensemble/models/base.py
+++ kwta_ensemble/models/base.py
@@ -15,6 +15,8 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """Implementation of model super class"""
+from copy import deepcopy
+import time
 from typing import Dict, Tuple
 
 import torch
@@ -43,3 +45,74 @@ class Model(torch.nn.Module):
 
     def epoch_train(self, data_loaders: Dict, phase: str) -> Tuple[float, float]:
         raise NotImplementedError
+
+    def fit(
+        self,
+        train_loader: torch.utils.data.DataLoader,
+        valid_loader: torch.utils.data.DataLoader,
+        epochs: int,
+        show_every: int = 2,
+    ) -> None:
+        """
+        Trains the model for a given epochs.
+
+        Parameters
+        ----------
+        train_loader: torch.utils.data.DataLoader
+            The training data loader to use.
+        valid_loader: torch.utils.data.DataLoader
+            The validation data loader to use.
+        epochs: int
+            The number of epochs to train the model.
+        show_every: int
+            The interval between training results displays.
+        """
+        start_time = time.time()
+        best_model_weights = deepcopy(self.state_dict())
+        best_accuracy = 0.0
+
+        data_loaders = {"train": train_loader, "valid": valid_loader}
+
+        for epoch in range(epochs):
+            if (epoch + 1) % show_every == 0:
+                print()
+                print(f"Epoch {epoch + 1}/{epochs}")
+                print("-" * 40)
+            for phase in data_loaders.keys():
+                if phase == "train":
+                    self.train()
+                else:
+                    self.eval()
+
+                epoch_loss, epoch_accuracy = self.epoch_train(
+                    data_loaders=data_loaders, phase=phase
+                )
+
+                if phase == "train":
+                    self.scheduler.step(epoch)
+
+                if phase == "train":
+                    self.train_loss.append(epoch_loss)
+                    self.train_accuracy.append(epoch_accuracy)
+                elif phase == "valid":
+                    self.valid_loss.append(epoch_loss)
+                    self.valid_accuracy.append(epoch_accuracy)
+
+                if (epoch + 1) % show_every == 0:
+                    print(
+                        f"{phase.title()} Loss: {epoch_loss:.4f} Acc: {epoch_accuracy:.4f}"
+                    )
+
+                if phase == "valid" and epoch_accuracy > best_accuracy:
+                    best_accuracy = epoch_accuracy
+                    best_model_weights = deepcopy(self.state_dict())
+
+        time_elapsed = time.time() - start_time
+        print()
+        print(
+            f"Training complete in {(time_elapsed // 60):.0f}m {(time_elapsed % 60):.0f}s"
+        )
+        print(f"Best Validation Accuracy: {best_accuracy:.4f}")
+        print()
+
+        self.load_state_dict(best_model_weights)
