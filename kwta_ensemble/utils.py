e3ca6931c41c06138c30bf92d64b3461a66c96e4
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-30 09:20:09 +0800

    add utility functions module

---
+++ kwta_ensemble/utils.py
@@ -0,0 +1,38 @@
+# k-Winners-Take-All Ensemble Neural Network
+# Copyright (C) 2021  Abien Fred Agarap
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""Utility functions module"""
+import random
+
+import numpy as np
+import torch
+
+
+def set_global_seed(seed: int) -> None:
+    """
+    Sets the seed value for random number generators.
+
+    Parameter
+    ---------
+    seed : int
+        The seed value to use.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
