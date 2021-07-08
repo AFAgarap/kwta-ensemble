fa4b0415ddfa8cd60eec785ea1b8c3166cdba6f5
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-07-08 18:26:52 +0800

    add base module

---
+++ modules/moe_classifier.py
@@ -0,0 +1,14 @@
+import argparse
+
+
+def main(arguments: argparse.Namespace):
+    pass
+
+
+def parse_args():
+    pass
+
+
+if __name__ == "__main__":
+    arguments = parse_args()
+    main(arguments)
