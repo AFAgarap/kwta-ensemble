024fc3f2fecc0da84724ff0650a0f6bf41676a16
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-07-08 18:30:21 +0800

    define argument parser

--- modules/moe_classifier.py
+++ modules/moe_classifier.py
@@ -17,13 +17,106 @@
 """MoE classifier"""
 import argparse
 
+from moe.models import MoE
+
+from kwta_ensemble.models import DNN
+
 
 def main(arguments: argparse.Namespace):
     pass
 
 
 def parse_args():
-    pass
+    parser = argparse.ArgumentParser(description="MoE classifier")
+    group = parser.add_argument_group("Parameters")
+    group.add_argument(
+        "-s",
+        "--seeds",
+        nargs="+",
+        type=int,
+        required=False,
+        default=[1234, 42, 73],
+        help="the list of random seeds to use, default: [1234, 42, 73]",
+    )
+    group.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        default="mnist",
+        help="the dataset to use, default: [mnist]",
+    )
+    group.add_argument(
+        "-v",
+        "--vectorizer",
+        type=str,
+        default="ngrams",
+        help="the vectorizer to use, options: [ngrams (default) | tfidf]",
+    )
+    group.add_argument(
+        "-nr",
+        "--ngram_range",
+        nargs="+",
+        required=False,
+        default=(1, 5),
+        type=int,
+        help="the n-grams range to use for text vectorization, default: [(1, 5)]",
+    )
+    group.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=256,
+        help="the mini-batch size to use, default: [256]",
+    )
+    group.add_argument(
+        "--optimizer",
+        type=str,
+        default="sgd",
+        help="the optimization algorithm to use, options: [sgd (default) | adamw]",
+    )
+    group.add_argument(
+        "-lr",
+        "--learning_rate",
+        type=float,
+        default=3e-4,
+        help="the learning rate to use for optimization, default: [3e-4]",
+    )
+    group.add_argument(
+        "-wd",
+        "--weight_decay",
+        type=float,
+        default=1e-5,
+        help="the weight decay to use during optimization, default: [1e-5]",
+    )
+    group.add_argument(
+        "-e",
+        "--epochs",
+        type=int,
+        default=10,
+        help="the number of epochs to train the model, default: [10]",
+    )
+    group.add_argument(
+        "-se",
+        "--show_every",
+        type=int,
+        default=1,
+        help="the number of interval between training progress displays, default: [1]",
+    )
+    group.add_argument(
+        "-nl",
+        "--num_subnetworks",
+        type=int,
+        default=3,
+        help="the number of subnetworks to instantiate, default: [3]",
+    )
+    group.add_argument(
+        "--subnetwork_architecture",
+        type=str,
+        default="dnn",
+        help="the architecture to use for an expert, options: [cnn | dnn (default) | lenet]",
+    )
+    arguments = parser.parse_args()
+    return arguments
 
 
 if __name__ == "__main__":
